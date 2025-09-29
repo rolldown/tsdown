f97429534972ab42580f641057b91f1af54460eb
Kevin Deng <sxzz@sxzz.moe>
2025-09-29 14:23:10 +0800

    refactor

--- src/config/types.ts
+++ src/config/types.ts
@@ -474,13 +474,15 @@ export type ResolvedConfig = Overwrite<
   MarkPartial<
     Omit<
       UserConfig,
-      | 'publicDir'
-      | 'workspace'
-      | 'silent'
-      | 'logLevel'
-      | 'failOnWarn'
-      | 'customLogger'
-      | 'fromVite'
+      | 'workspace' // merged
+      | 'fromVite' // merged
+      | 'publicDir' // deprecated
+      | 'silent' // deprecated
+      | 'bundle' // deprecated
+      | 'removeNodeProtocol' // deprecated
+      | 'logLevel' // merge to `logger`
+      | 'failOnWarn' // merge to `logger`
+      | 'customLogger' // merge to `logger`
     >,
     | 'globalName'
     | 'inputOptions'
@@ -493,11 +495,9 @@ export type ResolvedConfig = Overwrite<
     | 'fixedExtension'
     | 'outExtensions'
     | 'hooks'
-    | 'removeNodeProtocol'
     | 'copy'
     | 'loader'
     | 'name'
-    | 'bundle'
     | 'banner'
     | 'footer'
   >,
@@ -507,7 +507,7 @@ export type ResolvedConfig = Overwrite<
     clean: string[]
     dts: false | DtsOptions
     report: false | ReportOptions
-    tsconfig: string | false
+    tsconfig: false | string
     pkg?: PackageJson
     exports: false | ExportsOptions
     nodeProtocol: 'strip' | boolean
