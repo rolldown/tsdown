088405ad6c2570eb2cf170e63ca0ae089c00441c
Doctor Wu <wuzhouchun@qq.com>
2025-11-27 10:42:24 +0800

    chore: tweak functions to dedicated file

---
+++ packages/migrate/src/helpers/package-json.ts
@@ -0,0 +1,62 @@
+import { existsSync } from 'node:fs'
+import { readFile, writeFile } from 'node:fs/promises'
+import consola from 'consola'
+import { createPatch } from 'diff'
+import { detectIndentation } from '../../../../src/utils/format.ts'
+import { outputDiff, renameKey } from '../utils.ts'
+
+export async function migratePackageJson(
+  dryRun?: boolean,
+  depFields: Record<string, unknown> = {},
+): Promise<boolean> {
+  if (!existsSync('package.json')) {
+    consola.error('No package.json found')
+    return false
+  }
+
+  const pkgRaw = await readFile('package.json', 'utf8')
+  let pkg = JSON.parse(pkgRaw)
+  let found = false
+
+  for (const [field, semver] of Object.entries(depFields)) {
+    if (pkg[field]?.tsup) {
+      consola.info(`Migrating \`${field}\` to tsdown.`)
+      found = true
+      pkg[field] = renameKey(pkg[field], 'tsup', 'tsdown', semver)
+    }
+  }
+
+  if (pkg.scripts) {
+    for (const key of Object.keys(pkg.scripts)) {
+      if (pkg.scripts[key].includes('tsup')) {
+        consola.info(`Migrating \`${key}\` script to tsdown`)
+        found = true
+        pkg.scripts[key] = pkg.scripts[key].replaceAll(
+          /tsup(?:-node)?/g,
+          'tsdown',
+        )
+      }
+    }
+  }
+  if (pkg.tsup) {
+    consola.info('Migrating `tsup` field in package.json to `tsdown`.')
+    found = true
+    pkg = renameKey(pkg, 'tsup', 'tsdown')
+  }
+
+  if (!found) {
+    consola.warn('No tsup-related fields found in package.json')
+    return false
+  }
+
+  const eol = pkgRaw.endsWith('\n') ? '\n' : ''
+  const newPkgRaw = `${JSON.stringify(pkg, null, detectIndentation(pkgRaw))}${eol}`
+  if (dryRun) {
+    consola.info('[dry-run] package.json:')
+    outputDiff(createPatch('package.json', pkgRaw, newPkgRaw))
+  } else {
+    await writeFile('package.json', newPkgRaw)
+    consola.success('Migrated `package.json`')
+  }
+  return true
+}
