ebaf82c5b528601c74924d51b1f3fe4679d282d8
Liuigi <glbrt.liu@gmail.com>
2025-12-18 20:40:37 -0500

    feat(migrate): add monorepo support for tsup migration script
    Resolves #452

--- packages/migrate/src/index.ts
+++ packages/migrate/src/index.ts
@@ -1,6 +1,8 @@
+import path from 'node:path'
 import process from 'node:process'
 import { getCliCommand, parseNi, run } from '@antfu/ni'
-import { green, underline } from 'ansis'
+import { getPackagesSync } from '@manypkg/get-packages'
+import { green, greenBright, underline } from 'ansis'
 import consola from 'consola'
 import { migratePackageJson } from './helpers/package-json.ts'
 import { migrateTsupConfig } from './helpers/tsup-config.ts'
@@ -29,11 +31,53 @@ export async function migrate({ cwd, dryRun }: MigrateOptions): Promise<void> {
 
   if (cwd) process.chdir(cwd)
 
-  let migrated = await migratePackageJson(dryRun)
-  if (await migrateTsupConfig(dryRun)) {
-    migrated = true
+  const traverseStartCwd = process.cwd()
+  const { rootPackage, packages } = getPackagesSync(traverseStartCwd)
+  const cwds = new Set([traverseStartCwd])
+  // There are 3 possible cases here:
+  // 1. The current directory is a package root without a monorepo configuration.
+  //   In this case, `packages` will contain only the current directory. We use a
+  //   Set to avoid running it twice.
+  // 2. The current directory is not a package root, which means it is an internal
+  //   package within a monorepo. In this situation, we assume the user intentionally
+  //   specified the migration scope, so we operate only on the current directory.
+  // 3. The current directory is a package root and has a monorepo configuration.
+  //   In this case, `packages` contains only the discovered subpackage directories,
+  //   and we are going to include them.
+  if (rootPackage?.dir === traverseStartCwd) {
+    for (const { dir } of packages) {
+      cwds.add(dir)
+    }
   }
-  if (!migrated) {
+
+  let migratedAny = false
+
+  try {
+    for (const dir of cwds) {
+      process.chdir(dir)
+
+      const relativeDirLabel = greenBright(
+        path.relative(traverseStartCwd, dir) || 'the current directory',
+      )
+      consola.info(`Processing ${relativeDirLabel}`)
+
+      let migrated = await migratePackageJson(dryRun)
+      if (await migrateTsupConfig(dryRun)) {
+        migrated = true
+      }
+
+      if (!migrated) {
+        consola.info(`No migrations to apply in ${relativeDirLabel}.`)
+        continue
+      }
+
+      migratedAny = true
+    }
+  } finally {
+    process.chdir(traverseStartCwd)
+  }
+
+  if (!migratedAny) {
     consola.error('No migration performed.')
     process.exitCode = 1
     return
