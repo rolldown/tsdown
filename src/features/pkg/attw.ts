e099f70709b91408856eb8517ff31d6fe251e7e8
Doctor Wu <wuzhouchun@qq.com>
2025-12-20 10:03:03 +0800

    fix(attw): add --ignore-scripts flag to avoid lifecycle output
    close #658

--- src/features/pkg/attw.ts
+++ src/features/pkg/attw.ts
@@ -74,7 +74,7 @@ export async function attw(options: ResolvedConfig): Promise<void> {
   try {
     const { stdout: tarballInfo } = await exec(
       'npm',
-      ['pack', '--json', '--pack-destination', tempDir],
+      ['pack', '--json', '--ignore-scripts', '--pack-destination', tempDir],
       { nodeOptions: { cwd: options.cwd } },
     )
     const parsed = JSON.parse(tarballInfo)
