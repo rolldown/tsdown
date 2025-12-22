67eab42eeb1e1974a62da7995ab0ced09374aa4b
Chris <hizyyv@gmail.com>
2025-12-22 16:29:48 +0800

    feat(attw): add `ignoreRules` option to filter specified rule

--- src/features/pkg/attw.ts
+++ src/features/pkg/attw.ts
@@ -11,10 +11,26 @@ import type {
   CheckPackageOptions,
   CheckResult,
   Problem,
+  ProblemKind,
 } from '@arethetypeswrong/core'
 
 const debug = createDebug('tsdown:attw')
 const label = dim`[attw]`
+
+const problemFlags: Record<ProblemKind, string> = {
+  NoResolution: 'no-resolution',
+  UntypedResolution: 'untyped-resolution',
+  FalseCJS: 'false-cjs',
+  FalseESM: 'false-esm',
+  CJSResolvesToESM: 'cjs-resolves-to-esm',
+  FallbackCondition: 'fallback-condition',
+  CJSOnlyExportsDefault: 'cjs-only-exports-default',
+  NamedExports: 'named-exports',
+  FalseExportDefault: 'false-export-default',
+  MissingExportEquals: 'missing-export-equals',
+  UnexpectedModuleSyntax: 'unexpected-module-syntax',
+  InternalResolutionError: 'internal-resolution-error',
+}
 
 export interface AttwOptions extends CheckPackageOptions {
   /**
@@ -39,6 +55,13 @@ export interface AttwOptions extends CheckPackageOptions {
    * @default 'warn'
    */
   level?: 'error' | 'warn'
+
+  /**
+   * List of problem types to ignore.
+   *
+   * @see problemFlags
+   */
+  ignoreRules?: (typeof problemFlags)[keyof typeof problemFlags][]
 }
 
 /**
@@ -59,7 +82,23 @@ export async function attw(options: ResolvedConfig): Promise<void> {
     options.logger.warn('attw is enabled but package.json is not found')
     return
   }
-  const { profile = 'strict', level = 'warn', ...attwOptions } = options.attw
+  const {
+    profile = 'strict',
+    level = 'warn',
+    ignoreRules = [],
+    ...attwOptions
+  } = options.attw
+
+  const invalid = ignoreRules.find(
+    (rule) => !Object.values(problemFlags).includes(rule),
+  )
+  if (invalid) {
+    options.logger.warn(
+      `attw config option 'ignoreRules' argument '${invalid}' is invalid. Allowed choices are ${Object.values(
+        problemFlags,
+      ).join(', ')}.`,
+    )
+  }
 
   const t = performance.now()
   debug('Running attw check')
@@ -96,6 +135,11 @@ export async function attw(options: ResolvedConfig): Promise<void> {
   let errorMessage: string | undefined
   if (checkResult.types) {
     const problems = checkResult.problems.filter((problem) => {
+      // Exclude ignored problem kinds
+      if (ignoreRules.includes(problemFlags[problem.kind])) {
+        return false
+      }
+
       // Only apply profile filter to problems that have resolutionKind
       if ('resolutionKind' in problem) {
         return !profiles[profile]?.includes(problem.resolutionKind)
