ae2ab109bda5fbb60442e312faee532a32c209f7
Kevin Deng <sxzz@sxzz.moe>
2026-03-08 03:43:44 +0800

    fix(css): match `?inline` in any query position
    `RE_CSS_INLINE` now matches `foo.css?x=1&inline` in addition to
    `foo.css?inline`, aligning with Vite's `[?&]inline` pattern.

--- packages/css/src/utils.ts
+++ packages/css/src/utils.ts
@@ -3,7 +3,7 @@ export const RE_INLINE: RegExp = /[?&]inline\b/
 export const CSS_LANGS_RE: RegExp =
   /\.(?:css|less|sass|scss|styl|stylus)(?:$|\?)/
 export const RE_CSS_INLINE: RegExp =
-  /\.(?:css|less|sass|scss|styl|stylus)\?inline\b/
+  /\.(?:css|less|sass|scss|styl|stylus)\?(?:.*&)?inline\b/
 
 export function getCleanId(id: string): string {
   const queryIndex = id.indexOf('?')
