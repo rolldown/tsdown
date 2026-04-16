38afd8f1ad17e65e02e11da7aff6afad32922469
Kevin Deng <sxzz@sxzz.moe>
2026-04-16 17:12:58 +0900

    fix: skip Node.js version check in Bun

--- src/run.ts
+++ src/run.ts
@@ -5,9 +5,9 @@ import { yellow } from 'ansis'
 import lt from 'semver/functions/lt.js'
 import { runCLI } from './cli.ts'
 
-if (lt(process.version, '22.18.0')) {
+if (!process.versions.bun && lt(process.version, '22.18.0')) {
   console.warn(
-    yellow`[tsdown] Node.js ${process.version} is deprecated. Support will be removed in the next minor release. Please upgrade to Node.js 22.18.0 or later.`,
+    yellow`[tsdown] Node.js ${process.version} is deprecated. Support will be removed in the next minor release. Please upgrade to Node.js v22.18.0 or later.`,
   )
 }
 
