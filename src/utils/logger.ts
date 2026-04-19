ad01d2f7849ac9eaa894384d737cb457974116a8
Ilya <iosdsbb@gmail.com>
2026-04-19 18:35:46 +0300

    refactor(logger): replace ansis to style (node api)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,15 @@
 import process from 'node:process'
 import readline from 'node:readline'
-import { bgRed, bgYellow, blue, green, rgb, yellow, type Ansis } from 'ansis'
 import { noop } from './general.ts'
+import {
+  bgRed,
+  bgYellow,
+  blue,
+  colorize,
+  green,
+  yellow,
+  type StyleTextFormat,
+} from './style.ts'
 import type { InternalModuleFormat } from 'rolldown'
 
 export type LogType = 'error' | 'warn' | 'info'
@@ -127,9 +135,12 @@ export function createLogger(
 
 export const globalLogger: Logger = createLogger()
 
-export function getNameLabel(ansis: Ansis, name?: string): string | undefined {
+export function getNameLabel(
+  color: StyleTextFormat,
+  name?: string,
+): string | undefined {
   if (!name) return undefined
-  return ansis(`[${name}]`)
+  return colorize(color, `[${name}]`)
 }
 
 export function prettyFormat(format: InternalModuleFormat): string {
@@ -149,61 +160,35 @@ export function prettyFormat(format: InternalModuleFormat): string {
 }
 
 // Copied from https://github.com/antfu/vscode-pnpm-catalog-lens - MIT License
-const colors = new Map<string, Ansis>()
-export function generateColor(name: string = 'default'): Ansis {
+const colorPalette = [
+  'blue',
+  'green',
+  'yellow',
+  'magenta',
+  'cyan',
+  'redBright',
+  'greenBright',
+  'yellowBright',
+  'blueBright',
+  'magentaBright',
+  'cyanBright',
+] satisfies StyleTextFormat[]
+
+const colors = new Map<string, StyleTextFormat>()
+export function generateColor(name: string = 'default'): StyleTextFormat {
   if (colors.has(name)) {
     return colors.get(name)!
   }
-  let color: Ansis
+  let color: StyleTextFormat
   if (name === 'default') {
-    color = blue
+    color = 'blue'
   } else {
     let hash = 0
     for (let i = 0; i < name.length; i++)
       // eslint-disable-next-line unicorn/prefer-code-point
       hash = name.charCodeAt(i) + ((hash << 5) - hash)
-    const hue = hash % 360
-    const saturation = 35
-    const lightness = 55
-    color = rgb(...hslToRgb(hue, saturation, lightness))
+    color = colorPalette[Math.abs(hash) % colorPalette.length]
   }
   colors.set(name, color)
   return color
-}
-
-function hslToRgb(
-  h: number,
-  s: number,
-  l: number,
-): [r: number, g: number, b: number] {
-  h = h % 360
-  h /= 360
-  s /= 100
-  l /= 100
-  let r, g, b
-
-  if (s === 0) {
-    r = g = b = l // achromatic
-  } else {
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s
-    const p = 2 * l - q
-    r = hue2rgb(p, q, h + 1 / 3)
-    g = hue2rgb(p, q, h)
-    b = hue2rgb(p, q, h - 1 / 3)
-  }
-
-  return [
-    Math.max(0, Math.round(r * 255)),
-    Math.max(0, Math.round(g * 255)),
-    Math.max(0, Math.round(b * 255)),
-  ]
-}
-
-function hue2rgb(p: number, q: number, t: number) {
-  if (t < 0) t += 1
-  if (t > 1) t -= 1
-  if (t < 1 / 6) return p + (q - p) * 6 * t
-  if (t < 1 / 2) return q
-  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
-  return p
 }
