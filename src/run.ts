cb997322fdb5db469a027c8e91ef89dbc9aa8601
Ilya <iosdsbb@gmail.com>
2026-04-19 18:34:50 +0300

    refactor(run): replace ansis to style (node api)

--- src/run.ts
+++ src/run.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import module from 'node:module'
 import process from 'node:process'
-import { yellow } from 'ansis'
 import lt from 'semver/functions/lt.js'
 import { runCLI } from './cli.ts'
+import { yellow } from './utils/style.ts'
 
 if (!process.versions.bun && lt(process.version, '22.18.0')) {
   console.warn(
